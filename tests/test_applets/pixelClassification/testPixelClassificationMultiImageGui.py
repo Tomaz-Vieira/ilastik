0e1862d187a2d25d0c620aecb56e65e3aea1189d
Tomaz Vieira <tomaz.vieira@embl.de>
2019-09-26 17:37:29 +0200

    Adds live update gui tests
    Co-authored-by: Dominik Kutra <k-dominik@users.noreply.github.com>

--- tests/test_applets/pixelClassification/testPixelClassificationMultiImageGui.py
+++ tests/test_applets/pixelClassification/testPixelClassificationMultiImageGui.py
@@ -363,6 +363,65 @@ class TestPixelClassificationGuiMultiImage(ShellGuiTestCaseBase):
         self.exec_in_shell(impl)
 
 
+    def test_7_SwitchImages_live_update_consitency(self):
+        """
+        Switch back and forth between a labeled image and an unlabeled one.  Labels should disappear and then reappear.
+        """
+
+        def assert_not_in_live_update_state(shell, gui, pix_applet, image_index):
+            assert shell._currentImageIndex == image_index
+            current_gui = gui.currentGui()
+            assert not current_gui._labelControlUi.liveUpdateButton.isChecked()
+            assert pix_applet.topLevelOperator.FreezePredictions.value
+            assert current_gui._labelControlUi.AddLabelButton.isEnabled()
+            assert current_gui._labelControlUi.suggestFeaturesButton.isEnabled()
+
+        def assert_in_live_update_state(shell, gui, pix_applet, image_index):
+            assert shell._currentImageIndex == image_index
+            current_gui = gui.currentGui()
+            assert current_gui._labelControlUi.liveUpdateButton.isChecked()
+            assert not pix_applet.topLevelOperator.FreezePredictions.value
+            assert not current_gui._labelControlUi.AddLabelButton.isEnabled()
+            assert not current_gui._labelControlUi.suggestFeaturesButton.isEnabled()
+            assert current_gui._viewerControlUi.checkShowPredictions.isChecked()
+
+        def impl():
+            workflow = self.shell.projectManager.workflow
+            pixClassApplet = workflow.pcApplet
+            gui = pixClassApplet.getMultiLaneGui()
+            assert_not_in_live_update_state(self.shell, gui, pixClassApplet, 0)
+
+            # Select the second image
+            self.shell.imageSelectionCombo.setCurrentIndex(1)
+            gui.currentGui().editor.posModel.slicingPos = (0, 0, 0)
+            self.waitForViews(gui.currentGui().editor.imageViews)
+            assert_not_in_live_update_state(self.shell, gui, pixClassApplet, 1)
+
+            # switch on live update mode, in the second image
+            gui.currentGui()._labelControlUi.liveUpdateButton.click()
+            self.waitForViews(gui.currentGui().editor.imageViews)
+            assert_in_live_update_state(self.shell, gui, pixClassApplet, 1)
+
+            # Select the first image again
+            self.shell.imageSelectionCombo.setCurrentIndex(0)
+            gui.currentGui().editor.posModel.slicingPos = (0, 0, 0)
+            self.waitForViews(gui.currentGui().editor.imageViews)
+            assert_in_live_update_state(self.shell, gui, pixClassApplet, 0)
+
+            # disable live update mode again, this time in the first image
+            gui.currentGui()._labelControlUi.liveUpdateButton.click()
+            self.waitForViews(gui.currentGui().editor.imageViews)
+            assert_not_in_live_update_state(self.shell, gui, pixClassApplet, 0)
+
+            # Check if it worked in the second image
+            self.shell.imageSelectionCombo.setCurrentIndex(1)
+            gui.currentGui().editor.posModel.slicingPos = (0, 0, 0)
+            self.waitForViews(gui.currentGui().editor.imageViews)
+            assert_not_in_live_update_state(self.shell, gui, pixClassApplet, 1)
+
+        # Run this test from within the shell event loop
+        self.exec_in_shell(impl)
+
 if __name__ == "__main__":
     from tests.helpers.shellGuiTestCaseBase import run_shell_test
 
